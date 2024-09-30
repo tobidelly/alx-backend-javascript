597ea72b7ba6d9a31693183b2b64765d74e0c516
TD_SoftwareEngr <111124314+tobidelly@users.noreply.github.com>
2024-09-30 20:52:20 +0100

    Update 11-createEmployeesObject.js

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,2 +1,84 @@
+export default function createEmployeesObject(departmentName, employees) {
+  return {
+    [departmentName]: [...employees],
+  };
+}
 
-
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
