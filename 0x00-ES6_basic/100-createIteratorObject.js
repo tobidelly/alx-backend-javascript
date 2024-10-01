c63fc59162ac480236cad99c0fcd7c22ffd3dbfa
TD_SoftwareEngr <111124314+tobidelly@users.noreply.github.com>
2024-10-01 03:30:51 +0100

    Update 100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,2 +1,28 @@
+export default function createIteratorObject(report) {
+  const all = Object.values(report.allEmployees).reduce((a, b) => {
+    a.push(...b);
+    return a;  }, []);
+    let currIndex = 0;
+    const maxIndex = all.length;
+    return {
+      next() {
+        if (currIndex < maxIndex) {
+          const result = { value: all[currIndex], done: false };
+          currIndex += 1;
+          return result;
+        }
+        returns { value: null, done: true };
+      },
+      [Symbol.iterator]: () => this.next(),
+    };
+}
 
-
+
+
+
+
+
+
+
+
+
