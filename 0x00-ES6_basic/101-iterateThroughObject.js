707b24d29f11116198e4b5020e4cd3cd607fda56
TD_SoftwareEngr <111124314+tobidelly@users.noreply.github.com>
2024-10-01 03:33:15 +0100

    Update 101-iterateThroughObject.js

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,2 +1,62 @@
+export default function iterateThroughObject(reportWithIterator) {
+  let n = reportWithIterator.next();
+  let res = '';
+  while (!n.done) {
+    res += `${n.value} | `;
+    n = reportWithIterator.next();
+  }
+  
+  return res.slice(0, res.length - 3);
+}
 
-
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
